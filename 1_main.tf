737450775d2502c045e552f7bb9609b7cd2e88f4
StevenDelval <asteven59220@free.fr>
2026-02-10 10:59:49 +0100

    fix: remove acr

--- 1_main.tf
+++ 1_main.tf
@@ -38,32 +38,32 @@ module "stream_analytics" {
   sql_admin_password      = var.sql_admin_password
 }
 
-module "make_docker_image" {
-  source     = "./modules/make_docker_image"
-  container_registry_name = "acre6sdelval"
-  resource_group_name = azurerm_resource_group.rg.name
-  location =  var.location
-  image_name = "event_hub_producers"
-  path_image = "${path.root}/_events_producers"
-}
+# module "make_docker_image" {
+#   source     = "./modules/make_docker_image"
+#   container_registry_name = "acre6sdelval"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   location =  var.location
+#   image_name = "event_hub_producers"
+#   path_image = "${path.root}/_events_producers"
+# }
 
-// Event producers
-module "container_producers" {
-  source     = "./modules/container_producers"
-  depends_on = [module.module_event_hubs, module.stream_analytics, module.make_docker_image]
+# // Event producers
+# module "container_producers" {
+#   source     = "./modules/container_producers"
+#   depends_on = [module.module_event_hubs, module.stream_analytics, module.make_docker_image]
 
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
+#   location            = azurerm_resource_group.rg.location
 
-  container_image   = "${module.make_docker_image.login_server}/event_hub_producers"
-  connection_string = module.module_event_hubs.send_connection_string
+#   container_image   = "${module.make_docker_image.login_server}/event_hub_producers"
+#   connection_string = module.module_event_hubs.send_connection_string
 
-  acr_login_server = module.make_docker_image.login_server
-  acr_username = module.make_docker_image.admin_username
-  acr_password = module.make_docker_image.admin_password
-  sql_admin_password = var.sql_admin_password
-  sql_admin_user = var.sql_admin_login
-  sql_db = module.sql_database.database_name
-  sql_server = module.sql_database.server_fqdn
-}
+#   acr_login_server = module.make_docker_image.login_server
+#   acr_username = module.make_docker_image.admin_username
+#   acr_password = module.make_docker_image.admin_password
+#   sql_admin_password = var.sql_admin_password
+#   sql_admin_user = var.sql_admin_login
+#   sql_db = module.sql_database.database_name
+#   sql_server = module.sql_database.server_fqdn
+# }
 
